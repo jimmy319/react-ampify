d5676c294e615ec909397508f5cc8d151931da61
CR Jimmy <jimmy74319@gmail.com>
2018-05-06 22:56:56 +0800

    Fix indentation

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const convertToBindable = function (key) {
 
 module.exports = {
   /*
-	* Inline script element can't be created by using jsx syntax (security concern).
-	*	This function use dangerouslySetInnerHTML property to generate script element with json data for you.
-	*	@param json
-	*		json data which will be the inner content of script element
-	*	@return
-	*		script element with the given json data
-	*/
+  * Inline script element can't be created by using jsx syntax (security concern).
+  * This function use dangerouslySetInnerHTML property to generate script element with json data for you.
+  * @param json
+  *   json data which will be the inner content of script element
+  * @return
+  *   script element with the given json data
+  */
   createJsonScriptElement: function (json) {
     return (
       <script type='application/json' dangerouslySetInnerHTML={{__html: JSON.stringify(json)}} />
@@ -23,11 +23,11 @@ module.exports = {
   * For example: React will complain if you create element like this ... <input [value]='state.value' />.
   * This function help to append binding attributes to React element.
   * @param element
-  *		the react element which binding attributes will be attached to
-  *	@param attrs
-  *		the binding attributes
-  *	@return
-  *		the react element with binding attributes
+  *   the react element which binding attributes will be attached to
+  * @param attrs
+  *   the binding attributes
+  * @return
+  *   the react element with binding attributes
   */
   withBindingAttrs: function (element, attrs) {
     const elementProps = element.props
